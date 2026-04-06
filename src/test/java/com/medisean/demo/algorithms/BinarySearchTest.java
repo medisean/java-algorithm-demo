7ef73a18789dd006dc156e652abfa9f52eaf2a3e
medimedi <liangmingzou@163.com>
2026-04-06 19:51:59 +0800

    test: expand algorithm coverage

--- src/test/java/com/medisean/demo/algorithms/BinarySearchTest.java
+++ src/test/java/com/medisean/demo/algorithms/BinarySearchTest.java
@@ -12,8 +12,30 @@ class BinarySearchTest {
     }
 
     @Test
+    void shouldReturnBoundaryIndexes() {
+        int[] nums = {1, 3, 5, 7, 9, 11};
+
+        assertEquals(0, BinarySearch.search(nums, 1));
+        assertEquals(5, BinarySearch.search(nums, 11));
+    }
+
+    @Test
     void shouldReturnMinusOneWhenTargetDoesNotExist() {
         int[] nums = {1, 3, 5, 7, 9, 11};
         assertEquals(-1, BinarySearch.search(nums, 6));
+    }
+
+    @Test
+    void shouldReturnMinusOneForEmptyArray() {
+        int[] nums = {};
+        assertEquals(-1, BinarySearch.search(nums, 1));
+    }
+
+    @Test
+    void shouldWorkForSingleElementArray() {
+        int[] nums = {42};
+
+        assertEquals(0, BinarySearch.search(nums, 42));
+        assertEquals(-1, BinarySearch.search(nums, 7));
     }
 }
--- src/test/java/com/medisean/demo/algorithms/FibonacciTest.java
+++ src/test/java/com/medisean/demo/algorithms/FibonacciTest.java
@@ -17,7 +17,25 @@ class FibonacciTest {
     }
 
     @Test
+    void shouldHandleBaseCases() {
+        assertEquals(0, Fibonacci.dynamicProgramming(0));
+        assertEquals(1, Fibonacci.dynamicProgramming(1));
+        assertEquals(0, Fibonacci.recursive(0));
+        assertEquals(1, Fibonacci.recursive(1));
+    }
+
+    @Test
+    void shouldMatchKnownSequenceValues() {
+        int[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], Fibonacci.dynamicProgramming(i));
+        }
+    }
+
+    @Test
     void shouldRejectNegativeInput() {
         assertThrows(IllegalArgumentException.class, () -> Fibonacci.dynamicProgramming(-1));
+        assertThrows(IllegalArgumentException.class, () -> Fibonacci.recursive(-1));
     }
 }
